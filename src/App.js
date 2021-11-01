06c421870a7750dbb38c0ee70d6a04d35059db3a
Julio_Cesar_Pariona_Ventura <Julio_P@hotmail.com>
2021-11-01 13:39:12 -0500

    desafio -- Componentes I

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+import './components/css/App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <NavBar />
       </header>
-    </div>
+      <main>
+        <ItemListContainer />
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+const ItemListContainer = (props) => {
+  return (
+    <>
+      <h2 className="message">Bienvenidos a la pasteleria Yummy!</h2>
+    </>
+  )};
+export default ItemListContainer;
